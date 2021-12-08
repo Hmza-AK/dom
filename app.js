a2186493e8be8a5d292e5b10d5d87f1365478121
work complete <hamzaali.ke0315@gmail.com>
2021-12-09 00:43:38 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,86 @@
+// //q1
+// var main=document.getElementById("main-content")
+// //q2
+// console.log(main.childNodes);
+
+
+//q3
+// var fname=main.firstChild.innerHTML+="Hamza"
+// var lname=main.firstChild.nextSibling.innerHTML+="Ali Khan"
+// var email=main.firstChild.nextSibling.nextSibling.innerHTML+="hamzaali.ke0315@gmail.com"
+// var country=main.firstChild.nextSibling.nextSibling.nextSibling.innerHTML+="PAKISTAN"
+// var contact=main.lastChild.innerHTML+="0315-2991887"
+
+
+
+//q4
+// var fname=document.getElementById("first-name")
+// fname.value="Hamza"
+// console.log(fname)
+
+// // q5
+// var lname=document.getElementById("last-name")
+// lname.value="Ali Khan"
+// console.log(lname)
+
+// var email=document.getElementById("email")
+// email.value="hamzaali.ke0315@gmail.com"
+// console.log(email)
+
+
+///////////////////////div 2////////////////////////
+
+
+//q1
+
+// var formType=document.getElementById("form-content");
+// console.log(formType.nodeType)
+
+
+//q2
+// var childType=document.getElementById("lastName");
+// console.log(childType.childNodes)
+// // console.log(childType.nodeType)
+
+
+//q3
+// var update=document.getElementById("lastName");
+// update.innerHTML+="hamza"
+// console.log(update)
+
+
+
+//q4
+// var first=document.getElementById("main-content");
+// console.log(first.firstChild);
+// console.log(first.lastChild);
+
+//q5
+// var first=document.getElementById("lastName");
+// console.log(first.previousSibling);
+// console.log(first.nextSibling);
+
+
+//q6-1
+// var parent=document.getElementsByClassName("render");
+// console.log(parent.parentNode);
+
+
+// //q6-2
+// var email=document.getElementById("email");
+// console.log(email.parentNode)
+// var email=document.getElementById("email");
+// console.log(email.nodeType)
+
+
+
+
+
+
+
+
+
+
+
+
+
